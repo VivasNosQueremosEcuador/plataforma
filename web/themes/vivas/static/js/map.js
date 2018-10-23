7b829e5b6f4cb64300c2763e1ca85b7791a3ae12
Pame Pluas <pamepluas@gmail.com>
2018-10-22 22:10:17 -0500

    #000 [Pame] Agrega totales y faltantes en modal

--- web/themes/vivas/static/js/map.js
+++ web/themes/vivas/static/js/map.js
@@ -1,29 +1,35 @@
 var map = L.map('map-wrapper').setView([-1.8, -83], 6)
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  attribution: '© Contribuidores de <a href="http://openstreetmap.org">OpenStreetMap</a>',
-  minZoom: 6,
-  maxZoom: 15
+    attribution: '© Contribuidores de <a href="http://openstreetmap.org">OpenStreetMap</a>',
+    minZoom: 6,
+    maxZoom: 15
 }).addTo(map)
 
-var provincias = VIVAS.mapData.provincias
-
-var coordenadas = []
-
-for (var i = 0; i < provincias.length; i++) {
-    coordenadas.push(provincias[i].coordenadas)
-}
+const provincias = VIVAS.mapData.provincias
 
 console.log('VIVAS:', VIVAS.mapData);
 
 provincias.forEach(function (provincia) {
     L.marker(provincia.coordenadas).bindTooltip('Click para más información de ' + provincia.provincia)
         .on('click', function () {
-            document.getElementById('uk-modal-title').innerText = 'Provincia ' + provincia.provincia
-            var x = document.createElement("P");
-            var t = document.createTextNode("Totales:" + provincia.total)
-            x.appendChild(t);
-            document.getElementById('modal-body').appendChild(x)
+            document.getElementById('modal-title').innerText = 'Provincia ' + provincia.provincia
+            document.getElementById('modal-body').innerHTML = '<div class="uk-child-width-1-2@s uk-grid-match uk-text-center" uk-grid>' +
+                '<div>\n' +
+                '        <div class="uk-card uk-card-hover uk-card-small uk-card-body">\n' +
+                '            <h3 class="uk-card-title">' + provincia.total + '</h3>\n' +
+                '            <p>feminicidios contados</p>\n' +
+                '        </div>\n' +
+                '    </div>\n' +
+                '    <div>\n' +
+                '        <div class="uk-card uk-card-hover uk-card-small uk-card-body">\n' +
+                '            <h3 class="uk-card-title">' + provincia.faltante + '</h3>\n' +
+                '            <p>feminicidios con historias faltantes</p>\n' +
+                '        </div>\n' +
+                '    </div>' +
+                '</div>'
             UIkit.modal('#map-modal').show()
+            //TODO('Agregar link a categories/provincia')s
         }).addTo(map)
 })
 
+s
